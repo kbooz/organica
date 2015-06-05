6012fb7e3732787b0fe07caebb6efdd2de3f7451
Lucas Inacio de Souza <lucas1inacio7souza@gmail.com>
2015-06-05 19:40:26 -0300

    DOGE e Pastas

--- scroll/js/script.js
+++ scroll/js/script.js
@@ -1,7 +1,15 @@
+var keyf = 1500
 //Posição inicial
 window.onbeforeunload = function(){ window.scrollTo(0,0); }
 
 var s = skrollr.init({
-	render: function(data) {
+	smoothScrolling: true,
+	forceHeight: true,
+	constants:{
+		keyframe:keyf
 	}
 });
+
+// $("#down").click(function(){
+// 	skrollr.goTO
+// })
