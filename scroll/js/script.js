9d579fa8dbd76995f14a4882597b3e2515a506bf
Lucas Inacio de Souza <lucas1inacio7souza@gmail.com>
2015-06-06 21:27:19 -0300

    Adicionado SVG

--- scroll/js/script.js
+++ scroll/js/script.js
@@ -1,20 +1,11 @@
-var keyf = 1500
+// var keyf = 1500
 //Posição inicial
 window.onbeforeunload = function(){ window.scrollTo(0,0); }
 
 var s = skrollr.init({
 	smoothScrolling: true,
-	forceHeight: true,
-	constants:{
-		keyframe:keyf
-	}
-});
-
-$("#up").click(function (){
-	console.log($("#slide1").offset().top);
-	$("body").scrollTop(0);
-});
-
-$("#down").click(function (){
-	$("body").scrollTop(1510);
+	forceHeight: true
+	// constants:{
+	// 	keyframe:keyf
+	// }
 });
