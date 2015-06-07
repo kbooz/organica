f62ab84d6390e908bc894ccbf443d870b3144f0a
Lucas Inacio de Souza <lucas1inacio7souza@gmail.com>
2015-06-07 14:19:36 -0300

    Teste

--- scroll/js/script.js
+++ scroll/js/script.js
@@ -2,8 +2,7 @@
 //window.onbeforeunload = function(){ window.scrollTo(0,0); }
 
 var s = skrollr.init({
-	smoothScrolling: true,
-	forceHeight: true
+	smoothScrolling: true
 	// constants:{
 	// 	keyframe:keyf
 	// }
@@ -11,63 +10,63 @@ var s = skrollr.init({
 
 //Navegação do site
 
-// var $sections = $("section");
-// var ids = [];
-// var currentId=0;
-// var $nextButton = $("#next");
-// var $previousButton = $("#previous");
-//
-// $sections.each(function(){ids.push($(this).attr("id"))})
+var $sections = $("section");
+var ids = [];
+var currentId=0;
+var $nextButton = $("#next");
+var $previousButton = $("#previous");
 
-// $sections.waypoint(
-// 		function(direction) {
-// 	  if (direction === 'down') {
-// 			currentId = ids.indexOf(this.element.id);
-//
-// 			//Checa se é o ultimo elemento do array
-// 			if(currentId == ids.length-1)
-// 			{
-// 				//Desabilita o botão next
-// 				disableButton($nextButton);
-// 			}
-// 			if(currentId != 0)
-// 				activateButton($previousButton,-1);
-// 	  }
-// 	}, {
-// 	  offset: '25%'
-// 	}
-// );
-//
-// $sections.waypoint(
-// 		function(direction) {
-// 	  if (direction === 'up') {
-// 			currentId = ids.indexOf(this.element.id);
-//
-// 			//Checa se é o primeiro elemento do array
-// 			if(currentId == 0)
-// 			{
-// 				//Desabilita o botão next
-// 				disableButton($previousButton);
-// 			}
-// 			activateButton($nextButton,1);
-// 	  }
-// 	}, {
-// 	  offset: function() {
-// 	    return -this.element.clientHeight*0.5;
-// 	  }
-// 	}
-// );
+$sections.each(function(){ids.push($(this).attr("id"))})
 
-// function disableButton($button){
-// 	$button.addClass("disable");
-// 	$button.off();
-// }
-//
-// function activateButton($button,id){
-// 	$button.removeClass("disable");
-// 	$button.on("click",function(){
-// 		$('html, body').animate({scrollTop: $("#"+ids[currentId+id]).offset().top});
-// 	});
-// }
-//
-// activateButton($nextButton,1);
+$sections.waypoint(
+		function(direction) {
+	  if (direction === 'down') {
+			currentId = ids.indexOf(this.element.id);
+
+			//Checa se é o ultimo elemento do array
+			if(currentId == ids.length-1)
+			{
+				//Desabilita o botão next
+				disableButton($nextButton);
+			}
+			if(currentId != 0)
+				activateButton($previousButton,-1);
+	  }
+	}, {
+	  offset: '25%'
+	}
+);
+
+$sections.waypoint(
+		function(direction) {
+	  if (direction === 'up') {
+			currentId = ids.indexOf(this.element.id);
+
+			//Checa se é o primeiro elemento do array
+			if(currentId == 0)
+			{
+				//Desabilita o botão next
+				disableButton($previousButton);
+			}
+			activateButton($nextButton,1);
+	  }
+	}, {
+	  offset: function() {
+	    return -this.element.clientHeight*0.5;
+	  }
+	}
+);
+
+function disableButton($button){
+	$button.addClass("disable");
+	$button.off();
+}
+
+function activateButton($button,id){
+	$button.removeClass("disable");
+	$button.on("click",function(){
+		$('html, body').animate({scrollTop: $("#"+ids[currentId+id]).offset().top});
+	});
+}
+
+activateButton($nextButton,1);
